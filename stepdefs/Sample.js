6e6b2da95a0ec398787067115af3a225101cb36d
Santosh Marigoda <Santosh.Marigowda@mastek.com>
2015-03-16 10:48:47 +0000

    find multiple elements

--- stepdefs/Sample.js
+++ stepdefs/Sample.js
@@ -8,6 +8,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
 var assert = require('selenium-webdriver/testing/assert');
 
 var Steps = {
@@ -20,18 +21,25 @@ var Steps = {
 		});
 
 		library.when("I search for $query", function(query, next) {
-			ctx.driver.findElement(webdriver.By.css("#gbqfq")).sendKeys(query);
-			ctx.driver.findElement(webdriver.By.css("#gbqfb")).click().then(function() {
+			ctx.driver.findElement(webdriver.By.id("lst-ib")).sendKeys(query);
+			ctx.driver.findElement(webdriver.By.name("btnG")).click().then(function() {
 				next();
 			});
 		});
 
 		library.then("I should see the yadda github in the results", function(next) {
-			ctx.driver.findElement(webdriver.By.css("cite")).getText().then(function(text) {
-				assert(text).contains("https://github.com/acuminous/");
-	
+
+			var getText = function(text) { console.log(text)};
+			
+			var WebElement = ctx.driver.findElement(By.className("_Rm"));
+			console.log(WebElement.getText().then(getText))
+			// console.log(WebElement);
+			// WebElements.then(function(x) { x.map(function(d) { console.log(d.then(function(el) { el.getText()}))})});
+			// ctx.driver.findElements(By.className("_Rm")).getText().then(function(text) {
+			// 	console.log(text);
+			// 	// assert(text).contains("https://github.com/acuminous/");
 				next();
-			});
+			// });
 		});
 	}
 };
